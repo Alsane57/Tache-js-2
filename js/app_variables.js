52f87866f2fc0ca1b343fce39cbfd1a141b1f2d1
Alsane57 <136351035+Alsane57@users.noreply.github.com>
2024-01-16 15:24:52 +0100

    Add files via upload

---
+++ js/app_variables.js
@@ -0,0 +1,61 @@
+// // declaration
+// let prenom = 'Alou'; //String
+
+// let age = 20; //Number
+
+// let enligne = true; //Boolean
+
+// let choix = null; //Null
+
+// console.log(typeof prenom);
+// console.log(typeof age);
+// console.log(typeof enligne);
+// console.log(typeof choix);
+// console.log(typeof ville);
+
+// // concatenation
+// console.log( prenom + ' '  + age + 'ans' + ' ' + choix);
+// console.log( `je m'apelle ${prenom}, j'ai ${age} ans " `)
+
+// //Echapper un caracter antislash
+// console.log('il fait "beau ayjourd\'huit "')
+
+// age = 17;
+// console.log( `je m'apelle ${prenom}, j'ai ${age} ans " `)
+
+// VILLE = 'Dakar'
+
+// // Array 
+
+// let tab = [ 'Pommes' , 30 , true, ['bleu', 'rouge']]
+// // Gradeur du tableau
+// console.log(tab.length)
+
+// // index 1
+// console.log (tab [1])
+// // Mot rouge 
+// console.log (tab[3])
+ 
+
+// Déclarez les variables x, y et z
+let x = "1";
+let y = "2";
+let z = x + y;
+
+// Affichez le type et la valeur de chaque variable
+console.log("Type de x :", typeof x, ", Valeur de x :", x);
+console.log("Type de y :", typeof y, ", Valeur de y :", y);
+console.log("Type de z :", typeof z, ", Valeur de z :", z);
+
+// Forcez le type de x et y à entier
+x = parseInt(x);
+y = parseInt(y);
+
+// Exécutez à nouveau x + y
+let sum = x + y;
+
+// Affichez le type et la valeur de chaque variable après la conversion
+console.log("\nAprès conversion à entier :");
+console.log("Type de x :", typeof x, ", Valeur de x :", x);
+console.log("Type de y :", typeof y, ", Valeur de y :", y);
+console.log("Type de la somme :", typeof sum, ", Valeur de la somme :", sum);
